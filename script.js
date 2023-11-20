5a5a40d0430291b831253c42b06aa03cbae08d16
Regis-75018 <regisdesmarais@hotmail.fr>
2023-11-20 16:24:40 +0100

    nouveau dossier js

---
+++ script.js
@@ -0,0 +1,17 @@
+// const password = "guadeloupe"
+
+
+
+// let motUtilisateur = prompt("rentrez votre mot : " + password)
+
+// console.log(motUtilisateur);
+
+// if (password === motUtilisateur) {
+
+//     console.log("bien joué");
+
+// }
+// else {
+
+//     console.log("try again !!!");
+// }
